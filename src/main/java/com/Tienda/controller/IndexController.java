e471acdf09d239e4b7c53457c35824f1ead193e1
MijaDs <rmhborsig@gmail.com>
2023-11-01 01:55:45 -0600

    final semana 5

---
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.Tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author mija2
+ */
+@Controller
+public class IndexController {
+
+     @RequestMapping("/url")
+     public String page(Model model) {
+          model.addAttribute("attribute", "value");
+          return "view.name";
+     }
+
+}
---
+++ src/main/java/com/Tienda/domain/Categoria.java
@@ -0,0 +1,37 @@
+package com.Tienda.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data // creacion automatica de los atributos
+@Entity
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+
+     private static final long serialVersionUID = 1L;
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name = "id_categoria")//espesifica como se llama la columna en sql
+     //camelCase obligatorio
+     private Long idCategoria; //Transforma en id_categoria
+     private String descripcion;
+     private String rutaImagen;
+     private boolean activo;
+
+     public Categoria() {
+     }
+     //id se encarga la base de datos no se mete ene el constructor
+
+     public Categoria(String descripcion, String rutaImagen, boolean activo) {
+          this.descripcion = descripcion;
+          this.rutaImagen = rutaImagen;
+          this.activo = activo;
+     }
+
+}
