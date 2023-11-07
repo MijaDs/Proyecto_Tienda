a731101d0a1921466907b0d3d63989e7faed9e40
MijaDs <rmhborsig@gmail.com>
2023-11-07 15:23:54 -0600

    Tarea semana 8

--- src/main/java/com/Tienda/controller/IndexController.java
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-     @RequestMapping("/url")
+     @RequestMapping("/")
      public String page(Model model) {
           model.addAttribute("attribute", "value");
-          return "view.name";
+          return "index";
      }
 
 }
